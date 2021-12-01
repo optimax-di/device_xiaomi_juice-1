276c1f2b6ae6ccc186ebe683a8899b45e0dd3fe6
Zacky Marzuki <33113347+optimax-di@users.noreply.github.com>
2021-12-01 09:05:47 +0700

    Initial Juice Device Name

--- init/init_juice.cpp
+++ init/init_juice.cpp
@@ -72,25 +72,15 @@ void vendor_load_properties() {
         property_override(prop_name.c_str(), value.c_str(), false);
     };
     
-    property_override("ro.build.fingerprint", "google/sunfish/sunfish:11/RQ2A.210505.002/7246365:user/release-keys");
-    property_override("ro.build.description", "sunfish-user 11 RQ2A.210505.002 7246365 release-keys");
+    property_override("ro.build.fingerprint", "google/sunfish/sunfish:11/RQ2A.210405.005/7181113:user/release-keys");
+    property_override("ro.build.description", "sunfish-user 11 RQ2A.210405.005 7181113 release-keys");
 
     std::string region = GetProperty("ro.boot.hwc", "");
     std::string hwname = GetProperty("ro.boot.product.hardware.sku", "");
     if (hwname == "lime") {
-            if (region == "CN" || region =="Japan") {
-                property_override("ro.product.brand", "Redmi");
-                property_override("ro.product.model", "Redmi 9 Power");
-                property_override("ro.product.device", "lime");
-          } if (region == "India") {
-                property_override("ro.product.brand", "Redmi");
-                property_override("ro.product.model", "Redmi Note 9 4G");
-                property_override("ro.product.device", "lime");
-          } if (region == "Global") {
-                property_override("ro.product.brand", "Redmi");
-                property_override("ro.product.model", "Redmi 9T");
-                property_override("ro.product.device", "lime");
-          }     
+        property_override("ro.product.brand", "Redmi");
+        property_override("ro.product.model", "Redmi 9T");
+        property_override("ro.product.device", "lime");
     } else if (hwname == "lemon") {
         property_override("ro.product.brand", "Redmi");
         property_override("ro.product.model", "Redmi 9T");
